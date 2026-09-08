c450792071c2e31727eaf427f6b4d8c617fe72a6
Chethan M <chethan.ittasandra@gmail.com>
2026-09-08 17:05:23 +0530

    flow correction

--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/order/client/CustomerClient.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/order/client/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", path = "/api/customers")
+@FeignClient(name = "customer-service", url = "${customer.service.url}", path = "/api/customers")
 public interface CustomerClient {
 
     @GetMapping("/{id}")
--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/order/client/PaymentClient.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/order/client/PaymentClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", path = "/api/payments")
+@FeignClient(name = "payment-service", url = "${payment.service.url}", path = "/api/payments")
 public interface PaymentClient {
 
     @PostMapping
